9c367339311c02184162ac059b9f5a6c0a7c1753
Ostap Ronzhyn <ostap.ronzhyn@smashdocs.net>
2019-05-23 11:17:10 +0300

    Change icons and colors of system alerts

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 const validateType = function(type) {
   const types = ['info', 'warn', 'error', 'success', 'custom', 'smashdocs'];
   if (!type || type.length === 0 || types.includes(type) == false) {
-    console.warn('Invalid DropdownAlert type. Available types: info, warn, error, success or custom');
+    console.warn('Invalid DropdownAlert type. Available types: info, warn, error, success, custom or smashdocs');
     return false;
   }
   return true;
